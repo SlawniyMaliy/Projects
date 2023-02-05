c12d70e238669a8b7d6b306affb02fcd4b9344fd
Marty Mcfly <MatyM@xzy.com>
2023-02-05 23:15:48 +0300

    Add files chapter2

---
+++ Python/Books/Майкл Доусон/Chapter_2_Homework_Variable_names.py
@@ -0,0 +1,13 @@
+# Допустимые и не допустимые имена переменных
+# Списки имен переменных, допустимых (хорошие и плохие) и недопустимые
+valid_variables = ['Valid', 'name1', 'UpPerWoRd', 'UPPERWORD'
+                   'invalid_python_variable_names', '_name', 'adfkj',
+                   'total', 'DifferentСase_and_underscore', 'very_very_long_name']
+
+good_variables = ['name1', 'total', ]
+
+bad_variables = ['Valid', 'UpPerWoRd', 'UPPERWORD'
+                'invalid_python_variable_names', '_name', 'adfkj',
+                'DifferentСase_and_underscore', 'very_very_long_name']
+
+not_valid_variables = ['12valid', 'not valid', 'add-hock', '%int', 'print']
